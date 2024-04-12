2d4786582ca0e2955ccd4cc2ff59582678fc8a63
Huma-Taj07 <huma.tajmuh@gmail.com>
2024-04-12 12:21:23 -0700

    Initial commit

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Login.css";
+import video from "../../LoginAssets/video.mp4";
+import logo from "../../LoginAssets/logo.png";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div className="loginPage flex">
+      <div className="container flex">
+        <div className="videoDiv">
+          <video src={video} autoPlay muted loop></video>
+          <div className="textDiv">
+            <h2 className="title">Create and Sell Extraordinary Products</h2>
+            <p>Adop the peace of Nature!</p>
+          </div>
+          <div className="footerDiv">
+            <span className="text">Don't have any account?</span>
+            <Link to={"./register"}>
+              <button className="btn">Sign Up</button>
+            </Link>
+          </div>
+        </div>
+        <div className='formDiv flex'>
+        <div className="headerDiv">
+          <img src={logo} alt="Logo Image" />
+          <h3>Welcome Back</h3>
+        </div>
+
+        <form action="" className='form grid'>
+          <span>Login Status will go here</span>
+          <div className="inputDiv">
+
+          </div>
+
+        </form>
+
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
